ced19afe3cf298042f7d54f5360599f0fdace954
vishvadeep <vishvadeepsinghchauhan@gmail.com>
2023-12-18 13:24:03 +0530

    h

---
+++ 4A-javaScript-learning/style.js
@@ -0,0 +1,14 @@
+// const a = prompt("enter a no: ");
+// if (a % 2 == 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+let a = prompt("enter a no: ");
+if (a % 3 == 0 || a % 5 == 0) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
+
