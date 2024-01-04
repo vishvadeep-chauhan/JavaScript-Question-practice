f887f1c45a2463ae4be84315d4f759a56964b4c2
vishvadeep <vishvadeepsinghchauhan@gmail.com>
2024-01-05 02:57:21 +0530

    fgf

--- 4A-javaScript-learning/array/script.js
+++ 4A-javaScript-learning/array/script.js
@@ -4,7 +4,9 @@ let arr = [20, "drgsd", 45, false, {}]
 
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "nu
-    
+    if (arr[i] === Number(arr[i])) {
+        sum += arr[i];
+    }
+
 }
 console.log(sum);
