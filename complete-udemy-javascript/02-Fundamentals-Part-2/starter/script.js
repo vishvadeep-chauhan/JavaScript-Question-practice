6c31fb88a3b64e7e32a644e0f3419a8ae04fa6a7
vishvadeep <vishvadeepsinghchauhan@gmail.com>
2024-01-11 08:08:26 +0530

    Fix variable name and add comments

--- complete-udemy-javascript/02-Fundamentals-Part-2/starter/script.js
+++ complete-udemy-javascript/02-Fundamentals-Part-2/starter/script.js
@@ -1,8 +1,13 @@
-'use strict';
+// 'use strict';
 
 let hasDriversLicense = false;
 const passTest = true;
 
-if(passTest) hasDriversLicense = true;
+if(passTest) hasDriverLicense = true;
 if(hasDriversLicense) console.log('I can drive.');
 
+// --------------------------------------------------------
+
+const interface = 'audio';
+const private =  534;
+// cost if = 23;
