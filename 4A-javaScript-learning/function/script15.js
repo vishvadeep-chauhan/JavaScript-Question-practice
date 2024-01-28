58ca13214e3c8ce68341a3581f51481a2b1e2a92
vishvadeep <vishvadeepsinghchauhan@gmail.com>
2024-01-29 02:45:16 +0530

    Refactor code to use bind method for function context

--- 4A-javaScript-learning/function/script15.js
+++ 4A-javaScript-learning/function/script15.js
@@ -1,29 +1,29 @@
-// let student1 = {
-//   name: "reena",
-//   roll: 45,
-//   fees: 252556,
-//   marks: 233
-// };
+let student1 = {
+  name: "reena",
+  roll: 45,
+  fees: 252556,
+  marks: 233
+};
 
-// let student2 = {
-//   name: "tina" ,
-//   roll: 42,
-//   fees: 6465,
-//   marks: 341
-// }
+let student2 = {
+  name: "tina" ,
+  roll: 42,
+  fees: 6465,
+  marks: 341
+}
 
-// let getInfo = function (a, b, c) {
-//   console.log(this)
-//   console.log(this.name, this.roll, this.fees, this.marks)
-// };
+let getInfo = function (a, b, c) {
+  console.log(this)
+  console.log(this.name, this.roll, this.fees, this.marks)
+};
 
-// // getInfo();
+getInfo();
 
-// let getInfo1 = getInfo.bind(student1)
-// getInfo1()
+let getInfo1 = getInfo.bind(student1)
+getInfo1()
 
-// let getInfoSt2 = getInfo.bind(student2)
-// getInfoSt2()
+let getInfoSt2 = getInfo.bind(student2)
+getInfoSt2()
 
 
 // ----------------------------------------------------------------------
@@ -58,30 +58,31 @@
 
 // ----------------------------------------------------------------------------
 
-let student1 = {
-  name: "reena",
-  roll: 45,
-  fees: 252556,
-  marks: 233
-};
+// let student1 = {
+//   name: "reena",
+//   roll: 45,
+//   fees: 252556,
+//   marks: 233
+// };
 
-let student2 = {
-  name: "tina" ,
-  roll: 42,
-  fees: 6465,
-  marks: 341
-}
+// let student2 = {
+//   name: "tina" ,
+//   roll: 42,
+//   fees: 6465,
+//   marks: 341
+// }
 
-let getInfo = function (student) {
-  console.log(student);
-  console.log(student.name, student.roll, student.fees, student.marks);
-};
+// let getInfo = function (student) {
+//   console.log(student);
+//   console.log(student.name, student.roll, student.fees, student.marks);
+// };
 
-// getInfo(student1); 
+// // getInfo(student1); 
 
-let getInfoSt1 = getInfo.bind(null, student1);
-getInfoSt1();
+// let getInfoSt1 = getInfo.bind(null, student1);
+// getInfoSt1();
 
-let getInfoSt2 = getInfo.bind(null, student2);
-getInfoSt2();
+// let getInfoSt2 = getInfo.bind(null, student2);
+// getInfoSt2();
+
 
